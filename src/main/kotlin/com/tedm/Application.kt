edd56a5664bbbfdb7a19c19706ade145975ae0e9
Tadas <tadasgailevicius@yahoo.com>
2021-09-12 00:08:51 +0300

    Initial commit

---
+++ src/main/kotlin/com/tedm/Application.kt
@@ -0,0 +1,17 @@
+package com.tedm
+
+import io.ktor.application.*
+import com.tedm.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+}
