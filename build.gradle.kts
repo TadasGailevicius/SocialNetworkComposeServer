c1b4c02956ff67fba548f7b7f30be643cfdc9475
Tadas <tadasgailevicius@yahoo.com>
2021-09-14 22:20:18 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val koin_version: String by project
+val kmongo_version: String by project
 
 plugins {
     application
@@ -18,14 +20,35 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-websockets:$ktor_version")
-    implementation("io.ktor:ktor-gson:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
     implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+
+    // KMongo
+    implementation("org.litote.kmongo:kmongo:$kmongo_version")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
+
+    // Koin core features
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    // Test dependencies
+
+    // Gson
+    testImplementation("com.google.code.gson:gson:2.8.8")
+    // Koin
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+    // Ktor Test
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    // Kotlin Test
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    // Truth
+    testImplementation("com.google.truth:truth:1.1.3")
 }
--- src/main/kotlin/com/tedm/Application.kt
+++ src/main/kotlin/com/tedm/Application.kt
@@ -9,7 +9,6 @@ fun main(args: Array<String>): Unit =
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureRouting()
-    configureSockets()
     configureSerialization()
     configureMonitoring()
     configureHTTP()
