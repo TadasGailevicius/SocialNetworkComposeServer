8dc7dd1a9f31a501c37435415c1c2e0575b5137f
Tadas <tadasgailevicius@yahoo.com>
2021-09-15 21:34:26 +0300

    Add UserRoutes.kt Route

---
+++ src/main/kotlin/com/tedm/routes/UserRoutes.kt
@@ -0,0 +1,63 @@
+package com.tedm.routes
+
+import com.tedm.data.controller.user.UserController
+import com.tedm.data.models.User
+import com.tedm.data.requests.CreateAccountRequest
+import com.tedm.data.responses.BasicApiResponse
+import com.tedm.util.ApiResponseMessages.FIELDS_BLANK
+import com.tedm.util.ApiResponseMessages.USER_ALREADY_EXISTS
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import org.koin.ktor.ext.inject
+
+fun Route.userRoutes() {
+    val userController: UserController by inject()
+    route("/api/user/create") {
+        post {
+            val request = call.receiveOrNull<CreateAccountRequest>() ?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+            val userExists = userController.getUserByEmail(request.email) != null
+            if (userExists) {
+                call.respond(
+                    BasicApiResponse(
+                        successful = false,
+                        message = USER_ALREADY_EXISTS
+                    )
+                )
+                return@post
+            }
+            if (request.email.isBlank() || request.password.isBlank() || request.username.isBlank()) {
+                call.respond(
+                    BasicApiResponse(
+                        successful = false,
+                        message = FIELDS_BLANK
+                    )
+                )
+                return@post
+            }
+            userController.createUser(
+                User(
+                    email = request.email,
+                    username = request.username,
+                    password = request.password,
+                    profileImageUrl = "",
+                    bio = "",
+                    gitHubUrl = null,
+                    instagramUrl = null,
+                    linkedInUrl = null
+                )
+            )
+            call.respond(
+                BasicApiResponse(
+                    successful = true
+                )
+            )
+        }
+    }
+
+}
