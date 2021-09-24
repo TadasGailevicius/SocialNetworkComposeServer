9702a3e323ed30225d6ca46609b2e14f82e21b12
Tadas <tadasgailevicius@yahoo.com>
2021-09-25 00:15:52 +0300

    Add QueryParams.kt Constants

---
+++ src/main/kotlin/com/tedm/util/QueryParams.kt
@@ -0,0 +1,11 @@
+package com.tedm.util
+
+object QueryParams {
+
+    const val PARAM_PAGE = "page"
+    const val PARAM_PAGE_SIZE = "pageSize"
+    const val PARAM_POST_ID = "postId"
+    const val PARAM_QUERY = "query"
+    const val PARAM_USER_ID = "userId"
+    const val PARAM_PARENT_ID = "parentId"
+}
